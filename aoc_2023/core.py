a844381a3bdaf0d1839825ee425a464848a9341c
Aaron McUmber <aaron.mcumber@gmail.com>
2023-12-24 09:58:31 -0800

    feat: sketch problem for day 6

--- aoc_2023/core.py
+++ aoc_2023/core.py
@@ -1,7 +1,32 @@
 from pathlib import Path
+import re
 
 
 def read_input(file: Path) -> list[str]:
     """Read input.txt and return a list of stripped strings"""
     with open(file, "r") as fh:
         return [line.strip() for line in fh.readlines()]
+
+
+def parse_line(line: str, divider=":", idx=1) -> list[list[int]]:
+    """Parse a line"""
+    # TODO: change to re use if m.match ...
+    divided = line.split(divider)[idx]
+    return divided
+
+
+def parse_str_arr(array: str, divider: str = r"\s+") -> list[int]:
+    """Parse an array of ints in a string, separated by separator
+    Parameters
+    ----------
+    array : str
+        string array e.g. " 5   6   7  10"
+    divider : str
+        dividing char or RE
+
+    Returns
+    -------
+    list[int]
+        list of ints in str"""
+    m = re.compile(divider)
+    return [int(ele) for ele in m.split(array.strip())]
---
+++ aoc_2023/day05/part2/prototype.py
@@ -0,0 +1,79 @@
+"""Prototype for part 2 day 5"""
+import sys
+from pathlib import Path
+from typing import Iterable
+
+import matplotlib.pyplot as plt
+
+sys.path.append(str(Path(__file__).parent.resolve() / "../../.."))
+
+from aoc_2023.day05.day5 import SparseMap, get_maps, get_seeds
+
+from aoc_2023.day05.part2.solution import parse_seeds, get_paths
+
+
+def input_tbr():
+    return [
+        "seeds: 79 14 55 13",
+        "",
+        "seed-to-soil map:",
+        "50 98 2",
+        "52 50 48",
+        "",
+        "soil-to-fertilizer map:",
+        "0 15 37",
+        "37 52 2",
+        "39 0 15",
+        "",
+        "fertilizer-to-water map:",
+        "49 53 8",
+        "0 11 42",
+        "42 0 7",
+        "57 7 4",
+        "",
+        "water-to-light map:",
+        "88 18 7",
+        "18 25 70",
+        "",
+        "light-to-temperature map:",
+        "45 77 23",
+        "81 45 19",
+        "68 64 13",
+        "",
+        "temperature-to-humidity map:",
+        "0 69 1",
+        "1 0 69",
+        "",
+        "humidity-to-location map:",
+        "60 56 37",
+        "56 93 4",
+    ]
+
+
+def visualize(
+    seed_ranges: list[Iterable[int]], maps: dict[str, SparseMap]
+) -> tuple[plt.Figure, plt.Axes]:
+    """Visualize"""
+    x = []
+    y = []
+    for s_range in seed_ranges:
+        paths = get_paths(s_range, maps)
+        x.extend(list(paths.key()))
+        y.extend([path["location"] for path in paths.values()])
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(x, y)
+    return fig, ax
+
+
+def main(input_data: list[str]) -> tuple[plt.Figure, plt.Axes]:
+    """prototype"""
+    seeds = get_seeds(input_data)
+    seed_ranges = parse_seeds(seeds)
+    maps = get_maps(input_data)
+    fig, ax = visualize(seed_ranges, maps)
+    return fig, ax
+
+
+if __name__ == "__main__":
+    fig, ax = main(input_tbr())
+    plt.show()
