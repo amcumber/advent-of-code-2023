72903f0927d3a9c1bc6981ce784569cc316a61c1
Aaron McUmber <20608451+amcumber@users.noreply.github.com>
2023-12-30 15:59:05 -0700

    feat: complete 6.2

--- aoc_2023/day06/test_day6.py
+++ aoc_2023/day06/test_day6.py
@@ -1,6 +1,12 @@
 import pytest
 
-from aoc_2023.day06.day6 import BoatRace, get_races
+from aoc_2023.day06.day6 import (
+    BoatRace,
+    get_n_taus_above_record,
+    get_races,
+    parse_dists_p1,
+    parse_times_p1,
+)
 
 
 @pytest.fixture
@@ -28,6 +34,51 @@ def example_match3():
 
 def test_get_matches(example_input, example_match1, example_match2, example_match3):
     expected = [example_match1, example_match2, example_match3]
-    result = get_races(example_input)
+    result = get_races(example_input, parse_times_p1, parse_dists_p1)
     for r, e in zip(result, expected):
         r == e
+
+
+@pytest.mark.parametrize(
+    ["tau", "expected_dist", "boat_race"],
+    [
+        (0, 0, BoatRace(7, 9)),
+        (1, 6, BoatRace(7, 9)),
+        (2, 10, BoatRace(7, 9)),
+        (3, 12, BoatRace(7, 9)),
+        (4, 12, BoatRace(7, 9)),
+        (5, 10, BoatRace(7, 9)),
+        (6, 6, BoatRace(7, 9)),
+        (7, 0, BoatRace(7, 9)),
+    ],
+)
+def test_run_race(tau, expected_dist, boat_race):
+    result = boat_race.run_race(tau)
+    assert result == expected_dist
+
+
+@pytest.mark.parametrize(
+    ["boat_race", "expected"],
+    [
+        (BoatRace(7, 9), 3.5),
+        (BoatRace(15, 40), 7.5),
+    ],
+)
+def test_optimal_tau(boat_race, expected):
+    result = boat_race.get_optimal_tau()
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    ["boat_race", "expected"],
+    [
+        (BoatRace(7, 9), 4),
+        (BoatRace(15, 40), 8),
+        (BoatRace(30, 200), 9),
+    ],
+)
+def test_get_n_taus_above_record(boat_race, expected):
+    taus = boat_race.get_record_taus()
+    opt_tau = boat_race.get_optimal_tau()
+    results = get_n_taus_above_record(taus, opt_tau)
+    assert results == expected
